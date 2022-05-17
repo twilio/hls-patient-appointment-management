4c8298a7940f17b457eca739cccf08d9b051ca4b
ashikmeerankutty <ashik9591@gmail.com>
2022-05-17 11:44:13 +0530

    fix: add simulation event handlers for modify and reschedule

--- functions/deployment/simulation-event.js
+++ functions/deployment/simulation-event.js
@@ -17,14 +17,15 @@ async function createAppointment(context, appointment) {
     from: context.TWILIO_PHONE_NUMBER,
     parameters: appointment,
   };
+  console.log(appointment)
   try {
     const response = await context
       .getTwilioClient()
       .studio.flows(context.TWILIO_FLOW_SID)
       .executions.create(params);
-      const executionSid = response.sid;
+    const executionSid = response.sid;
   }
-  catch(err)  {
+  catch (err) {
     console.log("Error", err)
   }
 }
@@ -66,26 +67,52 @@ exports.handler = function (context, event, callback) {
 
   const response = new Twilio.Response();
   response.appendHeader('Content-Type', 'application/json');
+  const apptDatetime = new Date(tsTomorrow * 1000);
+  const appointment = {
+    event_type: event.command,
+    event_datetime_utc: null,
+    patient_id: '1000',
+    patient_first_name: event.firstName,
+    patient_last_name: 'Doe',
+    patient_phone: event.phoneNumber,
+    provider_id: 'afauci',
+    provider_first_name: 'Anthony',
+    provider_last_name: event.appointmentProvider,
+    provider_callback_phone: '(800) 555-2222',
+    appointment_location: event.appointmentLocation,
+    appointment_id: '20000',
+    appointment_timezone: '-0700',
+    appointment_datetime: apptDatetime.toISOString(),
+  };
+
   switch (event.command) {
     case 'BOOKED':
-      const apptDatetime = new Date(tsTomorrow * 1000);
+      console.log(event)
+      // Call studio flow with appointment
+      createAppointment(context, appointment)
+        .then(function () {
+          response.setBody({});
+          callback(null, response);
+        })
+        .catch(function (err) {
+          console.log(err);
+          callback(err, null);
+        });
+      break;
+    case 'MODIFIED':
+      // Call studio flow with appointment
+      createAppointment(context, appointment)
+        .then(function () {
+          response.setBody({});
+          callback(null, response);
+        })
+        .catch(function (err) {
+          console.log(err);
+          callback(err, null);
+        });
+      break;
 
-      const appointment = {
-        event_type: 'BOOKED',
-        event_datetime_utc: null,
-        patient_id: '1000',
-        patient_first_name: event.firstName,
-        patient_last_name: 'Doe',
-        patient_phone: event.phoneNumber,
-        provider_id: 'afauci',
-        provider_first_name: 'Anthony',
-        provider_last_name: 'Diaz',
-        provider_callback_phone: '(800) 555-2222',
-        appointment_location: 'Pacific Primary Care',
-        appointment_id: '20000',
-        appointment_timezone: '-0700',
-        appointment_datetime: apptDatetime.toISOString(),
-      };
+    case 'RESCHEDULED':
       // Call studio flow with appointment
       createAppointment(context, appointment)
         .then(function () {

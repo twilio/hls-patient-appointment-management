286a5c5944fe89e1defdb1c5d28aababeba470bb
ashikmeerankutty <ashik9591@gmail.com>
2022-06-08 13:18:43 +0530

    fix: limit to one sms per patient

--- functions/check-scheduled-reminders.js
+++ functions/check-scheduled-reminders.js
@@ -1,11 +1,19 @@
 async function checkScheduledReminders(context, event, callback) {
   const client = context.getTwilioClient();
   const allMessages = await client.messages.list();
-  const scheduledMessages = allMessages.filter(
-    ({ status, messagingServiceSid }) =>
+  const toSet = new Set();
+  const scheduledMessages = allMessages.reduce((acc, message) => {
+    const { status, messagingServiceSid, to } = message;
+    if (
       status === "scheduled" &&
-      messagingServiceSid === context.MESSAGING_SERVICE_SID
-  );
+      messagingServiceSid === context.MESSAGING_SERVICE_SID &&
+      !toSet.has(to)
+    ) {
+      toSet.add(to);
+      return [...acc, message];
+    }
+    return acc;
+  }, []);
   return callback(null, scheduledMessages);
 }
 
--- functions/send-scheduled-reminders.js
+++ functions/send-scheduled-reminders.js
@@ -1,28 +1,34 @@
 async function sendScheduledReminders(context, event, callback) {
   const client = context.getTwilioClient();
   const allMessages = await client.messages.list();
+  const toSet = new Set();
   const scheduledMessages = allMessages.filter(
     ({ status, messagingServiceSid }) =>
       status === "scheduled" &&
       messagingServiceSid === context.MESSAGING_SERVICE_SID
   );
-  const queuedMessages = scheduledMessages.map(
-    async ({ sid, body, to, messagingServiceSid }) => {
-      // Cancel the scheduled message
-      await client.messages(sid).update({ status: "canceled" });
-      return await client.messages.create({
+  console.log(scheduledMessages);
+  const toSendMessages = [];
+  for (const message of scheduledMessages) {
+    console.log(message)
+    const { sid, body, to, messagingServiceSid } = message;
+    await client.messages(sid).update({ status: "canceled" });
+    console.log("toSet", toSet);
+    if (!toSet.has(to)) {
+      toSet.add(to);
+      toSendMessages.push({ messagingServiceSid, body, to });
+    }
+  }
+  console.log(toSendMessages);
+  const results = await Promise.all(
+    toSendMessages.map(({ body, to, messagingServiceSid }) =>
+      client.messages.create({
         messagingServiceSid,
         body,
         to,
-      });
-    }
+      })
+    )
   );
-  let results = [];
-  try {
-    results = await Promise.all(queuedMessages);
-  } catch (err) {
-    console.log(err);
-  }
   return callback(null, results);
 }
 

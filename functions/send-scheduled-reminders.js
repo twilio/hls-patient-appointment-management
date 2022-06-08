8c3f6b949bcabb11dc984a0822b3f0718761d182
ashikmeerankutty <ashik9591@gmail.com>
2022-06-08 14:21:58 +0530

    fix: remove logs

--- functions/send-scheduled-reminders.js
+++ functions/send-scheduled-reminders.js
@@ -7,19 +7,15 @@ async function sendScheduledReminders(context, event, callback) {
       status === "scheduled" &&
       messagingServiceSid === context.MESSAGING_SERVICE_SID
   );
-  console.log(scheduledMessages);
   const toSendMessages = [];
   for (const message of scheduledMessages) {
-    console.log(message)
     const { sid, body, to, messagingServiceSid } = message;
     await client.messages(sid).update({ status: "canceled" });
-    console.log("toSet", toSet);
     if (!toSet.has(to)) {
       toSet.add(to);
       toSendMessages.push({ messagingServiceSid, body, to });
     }
   }
-  console.log(toSendMessages);
   const results = await Promise.all(
     toSendMessages.map(({ body, to, messagingServiceSid }) =>
       client.messages.create({
